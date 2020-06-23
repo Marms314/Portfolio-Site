5139dd982db8e06efcc3d74ef7d93aefe3e2b901
Marms <maryarmstrong314@gmail.com>
2020-06-23 14:10:36 -0500

    Added components for basic portfolio site. Deleted "app-" from their selectors in .ts files and added them to app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    AboutMeComponent,
+    ContactFormComponent,
+    PortfolioPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
